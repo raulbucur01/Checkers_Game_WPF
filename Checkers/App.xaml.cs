77a1cc6d39df97f4bfd39faabfe9ea24ace3025f
raulbucur01 <raul.bucur@student.unitbv.ro>
2024-04-22 10:42:00 +0300

    m

--- Checkers/App.xaml.cs
+++ Checkers/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 
 namespace Checkers
 {
@@ -6,6 +7,7 @@ namespace Checkers
     {
         protected override void OnStartup(StartupEventArgs e)
         {
+
             base.OnStartup(e);
         }
     }
